2276fe02989c7b45a36731fd3498f9d240a686c2
YuliaBondar <uliabondar359@gmail.com>
2024-08-24 01:00:39 +0300

    add gallery file with photo about buildings

--- src/OtherComponent/Gallery/Gallery.jsx
+++ src/OtherComponent/Gallery/Gallery.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
 import './Gallery.css'
+import gallery_element from'./Gallery.js'
 
 const Gallery = () => {
   return (
     <div className='gallery-item'>
-        
+    <div className="elements-gallery">
+        {gallery_element.map(item => (
+            <div key={item.id} className="gallery-element">
+              
+                <img src={item.src} alt='ldld'/>
+                <div className="items-about-building">
+                  <p className='item-name'><b>{item.name}</b></p>
+                  <p className='item-timing'>{item.timing}</p>
+                  <a href={item.link}> <button className='contact-button'>See process</button></a>
+            
+                </div>
+              </div>
+        ))}
     </div>
+    <button>See more</button>
+</div>
   )
 }
 
