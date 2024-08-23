3b75c63436e55bc382cf31e1fe8635d6f2f64750
YuliaBondar <uliabondar359@gmail.com>
2024-08-24 00:42:10 +0300

    add Gallery file.

---
+++ src/OtherComponent/Gallery/Gallery.js
@@ -0,0 +1,47 @@
+import Photo1 from './photo/photo1.png'
+import Photo2 from './photo/photo2.png'
+import Photo3 from './photo/photo3.png'
+import Photo4 from './photo/photo4.png'
+import Photo5 from './photo/photo5.png'
+import Photo6 from './photo/photo6.png'
+
+const gallery_element =[
+    {id:1,
+        name:'Interior design',
+        timing:'3 Months work time',
+        src:Photo1,
+        link:'',
+    },
+    {id:2,
+        name:'Build modern house',
+        timing:'3 Months work time',
+        src:Photo2,
+        link:'',
+    },
+    {id:3,
+        name:'Build villa two floors',
+        timing:'3 Months work time',
+        src:Photo3,
+        link:'',
+    },
+    {id:4,
+        name:'Modern interior design',
+        timing:'3 Months work time',
+        src:Photo4,
+        link:'',
+    },
+    {id:5,
+        name:'Renovation co working room',
+        timing:'3 Months work time',
+        src:Photo5,
+        link:'',
+    },
+    {id:6,
+        name:'Real estate house',
+        timing:'3 Months work time',
+        src:Photo6,
+        link:'',
+    },
+];
+
+export default gallery_element;
